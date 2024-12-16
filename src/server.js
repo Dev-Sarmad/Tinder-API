f23c1b50e9e0183c4f9755e94228436639707ddd
Dev-Sarmad <sarmadh230@gmail.com>
2024-12-16 12:11:24 +0500

    making post api for sending data into DB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 const express = require("express");
-//importing the function 
+//importing the function
 const connectionDB = require("./config/db");
+
+const User = require("./models/user");
 
 //initializing the app using the express
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  //creating the new instance of user model
+  const user = new User({
+    firstName: "Sam",
+    lastName: "habib",
+    email: "sam@gmail.com",
+    password: "alpha123",
+    age: 20,
+    gender: "male",
+  });
+  //this object will be saved in the database
+  //all of the mongoose functions either you are getting , savnig data into the database
+  // It will return you a promise so we use await here
+  await user.save();
+
+  res.send("user created successfully");
+});
 
 //calling the connectionDB function and it will connects database
 //  and then it connects to the server
