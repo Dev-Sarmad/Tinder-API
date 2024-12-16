a711efb06bc8e9686963c023a7d402d12185a1b4
Dev-Sarmad <sarmadh230@gmail.com>
2024-12-16 12:52:35 +0500

    seding the json data dynamicaly

--- src/server.js
+++ src/server.js
@@ -7,22 +7,25 @@ const User = require("./models/user");
 //initializing the app using the express
 const app = express();
 
+//our express server does not accept the javascript object from the client so we use the middelware
+//app.use(this middleware will be used across all the application)
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   //creating the new instance of user model
-  const user = new User({
-    firstName: "Sam",
-    lastName: "habib",
-    email: "sam@gmail.com",
-    password: "alpha123",
-    age: 20,
-    gender: "male",
-  });
+  //earlier we were not sending the javascript object to the server via client
+  //instead we were creating the object on server itself here we will be sending data to the server
+  const user = new User(req.body);
+  console.log(user);
   //this object will be saved in the database
   //all of the mongoose functions either you are getting , savnig data into the database
   // It will return you a promise so we use await here
-  await user.save();
+  try {
+    await user.save();
 
-  res.send("user created successfully");
+    res.send("user created successfully");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //calling the connectionDB function and it will connects database
